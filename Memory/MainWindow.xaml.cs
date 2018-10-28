b87941a28638e18e90fd3dcd2e1817836a704ee3
Gijs Rijkers <gijs.rijkers.rijkers@student.nhlstenden.com>
2018-10-28 12:29:48 +0100

    v0.10 Main menu, navigation

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -22,12 +22,7 @@ namespace Memory
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int time = 59;
-
-        private DispatcherTimer Timer;
-        private const int NR_OF_COLS = 4;
-        private const int NR_OF_ROWS = 4;
-        MemoryGrid grid;
+        
         System.Media.SoundPlayer player = new System.Media.SoundPlayer(Properties.Resources.sound);
         //MemoryGrid ResetGrid;
 
@@ -35,66 +30,27 @@ namespace Memory
         public MainWindow()
         {
             InitializeComponent();
-            grid = new MemoryGrid(Gamegrid, NR_OF_COLS, NR_OF_ROWS);
-            //ResetGrid = new MemoryGrid();
-            Timer = new DispatcherTimer();
-            Timer.Interval = new TimeSpan(0,0,1);
-            Timer.Tick += Timer_Tick;
-            Timer.Start();
-            player.Play();
+           
 
         }
 
-
-        private void Timer_Tick(object sender, EventArgs e)
+        private void SingleplayerKlick(object sender, RoutedEventArgs e)
         {
-            if (time > 0)
-            {
-                if(time <= 10)
-                {
-                    if (time % 2 == 0)
-                    {
-                        TBCountDown.Foreground = Brushes.Red;
-                    }
-                    else
-                    {
-                        TBCountDown.Foreground = Brushes.White;
-                    }
-                }
-                time--;
-                TBCountDown.Text = String.Format("00:0{0}:{1}", time / 60, time % 60);
-            }
-            else
-            {
-                Timer.Stop();
-                // niet gelukt, andere oplossing (tijdelijk) gevonden 
-                //MemoryGrid t = ResetGrid;
-                MessageBox.Show("Out of time Druk op Ok om opnieuw te starten !");
-                // dit zorgt dat de huidige spel afgesloten
-                Application.Current.Shutdown();
-                // dit zorgt om opniew te beginnen
-                System.Windows.Forms.Application.Restart();
-                TimeSpan t = new TimeSpan();
-                
-
-            }
+            this.Hide();
+            Singleplayer SingelplayerWin = new Singleplayer(this);
+            SingelplayerWin.Show();
         }
 
-        private void btnStartPauze_Click(object sender, RoutedEventArgs e)
+        private void CloseApp(object sender, RoutedEventArgs e)
         {
-            btnStartPauze.Content = btnStartPauze.Content == "Start" ? "Pause" : "Start";
-            if(btnStartPauze.Content == "Start")
-            {
-                MessageBox.Show("Pause Game");
-                Timer.Stop();
-            }
-            else
-            {
-                MessageBox.Show("Start Game");
-                Timer.Start();
-                
-            }
-            
+            this.Close();
+        }
+
+        private void MulyiplayerClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Multiplayer multiplayerWin = new Multiplayer(this);
+            multiplayerWin.Show();
         }
     } 
 }
